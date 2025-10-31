93c6a41820cb77095533e5ea231222f98e06ecc8
Stick <142520621+SticksOnTheBeach@users.noreply.github.com>
2025-10-31 23:27:33 +0100

    Remove unnecessary XML comments and whitespace

--- VPSControl.xaml.cs
+++ VPSControl.xaml.cs
@@ -3,13 +3,12 @@ using System.Windows.Controls;
 
 namespace VPSControl
 {
-    /// <summary>
-    /// Interaction logic for VPSControl.xaml
-    /// </summary>
+
     public partial class VPSControl : UserControl
     {
         public VPSControl()
         {
         }
     }
+
 }
